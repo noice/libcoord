4fd2712a916c79c7d260bf90799b6436edff28f7
Oleg Demin <olegkastrategka@gmail.com>
2020-04-20 19:10:34 +0300

    New functions

--- src/libcoord.c
+++ src/libcoord.c
@@ -1,28 +1,49 @@
 // ----------------------------------- Headers ----------------------------------
-#include "libcoord.h"
+#include "libcoord.h"   /* Library declaration */
+#include "hash_table.h" /* Hash table */
 
-#include <sys/types.h>  /* Special types */
-#include <sys/socket.h> /* For connection */
-#include <netdb.h>      /* Network address and service translation */
-#include <string.h>     /* Memset */
-#include <stdlib.h>     /* Standard funcs */
-#include <stdio.h>      /* Formatted output */
-#include <unistd.h>     /* Syscalls */
-#include <stdbool.h>    /* Bools */
+#include <sys/types.h>    /* Special types */
+#include <sys/socket.h>   /* For connection */
+#include <netdb.h>        /* Network address and service translation */
+#include <string.h>       /* Memset */
+#include <stdlib.h>       /* Standard funcs */
+#include <stdio.h>        /* Formatted output */
+#include <unistd.h>       /* Syscalls */
+#include <signal.h>       /* Signal handling */
+#include <event2/event.h> /* libevent */
+
+// ----------------------------------- Defines ----------------------------------
+// Initial Hash table
+#define INITHASHTBLSIZE 5
 
 // ---------------------------------- Prototypes --------------------------------
 // Additional funcs
-int connect_to_coordinator();
+Module* new_module(mod_id, name_len, char *, int, int);
+int     connect_to_coordinator();
+int     init_modules_storage();
+void    get_fds(char *, msg_len, char *, char *);
+void    set_exit_handler();
+void    exit_handler(int);
+void    shutdown_module();
 
-int init_modules_storage();
+// --------------------------------- Global vars --------------------------------
+Hash_table *ht = NULL;        /* Hash table of Module */
+struct event_base* eb = NULL; /* Event base */
+
 // ---------------------------------- Main funcs --------------------------------
 /*
  * Initialize new module
  * Notify Coordinator about self-existence
  * Initialize modules' data storage
+ * Create event base
  */
 int
 init_module(char *name, char *addr, char *port) {
+    if (set_exit_handler() == -1) {
+       fprintf(stderr, "Error while setting exit handler\n"); 
+       return -1;
+    }
+    
     if (connect_to_coordinator(name, addr, port)) {
        fprintf(stderr, "Error while connecting to Coordinator\n"); 
        return -1;
@@ -30,18 +51,27 @@ init_module(char *name, char *addr, char *port) {
 
     init_modules_storage();
 
+    // Initialise Libevent
+    // Create event base
+    *eb = event_base_new();
+    if (!eb) {
+       fprintf(stderr, "Error while creating event base\n"); 
+       return -1;
+    }
+
     return 0;
 }
 
 /*
  * Get module info by provided @name
- * Check modules' data storage
- * If exist, returns pointer to Module 
  * Otherwise returns NULL
  */
 Module* 
 get_module_by_name(char *name) {
-    return NULL;
+    // Search Module in Hash table
+    // Return if exist
+    // NULL, otherwise
+    return (Module *) search_item(ht, name);
 }
 
 /*
@@ -51,8 +81,59 @@ get_module_by_name(char *name) {
  * If no, returns -1 (failure)
  */
 int
-connect_to_module(Module *mod) {
-    return 0;
+connect_to_module(char *name) {
+    // Connection request
+    // 7  = REQCONMSGSIZE + 1 (' ') + 1 ('\n') + 1 ('\0')
+    // 29 = ACCONMSGSIZE + 10 (id) + 10 (id) + 2 (' ') + 1 ('\n') + 1 ('\0')
+    char *request = (char *) malloc(sizeof(char)*(7 + strlen(name)));      
+    char *response[29];
+
+    // Construct request
+    sprintf(request, "%s %s\n", REQCONMSG, name);
+
+    // Get Coordinator's module
+    Module *coord = get_module_by_name(COORD);
+    
+    // Send connection request to Coordinator
+    if (send_msg(coord, request, strlen(request)) == -1) {
+        fprintf(stderr, "Error while sending msg to %s\n", name);
+        return -1;
+    }
+    
+    // Read response
+    read(coord->read_end, response, 29);
+
+    // If ACCONMSG, then get fds
+    if (strncmp(response, ACCONMSG, ACCONMSGSIZE) == 0) {
+        char write_end[10]; /* store write-end to Coordinator here */
+        char read_end[10];  /* store read-end to Coordinator here */
+        
+        get_fds(response, ACCONMSGSIZE, write_end, read_end); 
+
+        // Create new module with given fds
+        Module *req_module = new_module(name, -1, atoi(write_end), atoi(read_end));
+        // Then add it to Modules' storage
+        insert_item(ht, req_module->name, req_module);
+
+        // Check if is ready to read
+        event *ev = event_new(eb, req_module->read_end, EV_READ | EV_PERSIST);
+
+        // Check if is ready to write
+        event *ev = event_new(eb, req_module->write_end, EV_WRITE | EV_PERSIST);
+
+        return 0;
+    }
+    // If declined connection 
+    else if (strncmp(ans, DECCONMSG, DECCONMSGSIZE) == 0) {
+        fprintf(stderr, "Connection was declined\n"); 
+
+        return -1;
+    }
+    // Other messages also is error
+    // Return -1
+    else {
+        return -1;
+    }
 }
 
 /*
@@ -61,6 +142,17 @@ connect_to_module(Module *mod) {
  */
 int
 send_msg(Module *mod, char *buf, msg_len len) {
+    // If Module is NULL, return error
+    if (mod == NULL) {
+        fprintf(stderr, "Module doesn't exist\n");
+        return -1;
+    }
+    // Send msg
+    if (send(mod->write_end, buf, len, 0) == -1) {
+        perror("Error while sending msg to Module");
+        return -1;
+    }
+
     return 0;
 }
 
@@ -85,6 +177,21 @@ get_msg() {
     return NULL;
 }
 // ------------------------------- Additional funcs -----------------------------
+/*
+ * Return new Module
+ */
+Module*
+new_module(mod_id id, char *name, int write_end, int read_end) {
+    Module *mod = (Module *) malloc(sizeof(Module));
+
+    mod->id = id;
+    mod->name = name;
+    mod->write_end = write_end;
+    mod->read_end = read_end;
+
+    return mod;
+}
+
 /*
  * Send self module's name to coordinator (by default address)
  * Wait for connection to coordinator
@@ -141,13 +248,15 @@ connect_to_coordinator(char *name, char *addr, char *port) {
 
     freeaddrinfo(result);
 
-    // Send to Coordinator "IEXIST (module's name)"
-    char msg[264]; /* message to Coordinator*/
+    // Send to Coordinator "INITMSG (module's name)\n"
+    // (msg) 260 = INITMSGSIZE + 1 (' ') + 1 ('\n') + 255 (module's name)
+    // (ans) 27 = ACCEXISTMSGSIZE + 10 (write_end) + 10 (read_end) + 2 (' ') + 1 ('\n') + ('\0')
+    char msg[260]; /* message to Coordinator */
     char ans[27];  /* response */
     
-    sprintf(msg, "IEXIST (%s)\n", name);
+    sprintf(msg, "%s (%s)\n", INITMSG, name);
     if (send(sfd, msg, strlen(msg), 0) == -1) {
-        perror("Error while sending \"IEXIST\" to Coordinator");
+        perror("Error while sending INITMSG to Coordinator");
         close(sfd);
         return -1;
     }
@@ -157,39 +266,25 @@ connect_to_coordinator(char *name, char *addr, char *port) {
 
     // TODO: Perhaps in future this will be in separate function
     // If LOL, then everything is alright
-    if (strncmp(ans, "LOL", 3) == 0) {
+    if (strncmp(ans, ACCEXISTMSG, ACCEXISTMSGSIZE) == 0) {
         char write_end[10]; /* store write-end to Coordinator here */
         char read_end[10];  /* store read-end to Coordinator here */
+
+        // Get fds from answer
+        get_fds(ans, ACCEXISTMSGSIZE, write_end, read_end);
         
-        int i, j = 0;
+        // Create new module with given fds
+        Module *coord = new_module(COORD, 0, atoi(write_end), atoi(read_end));
 
-        bool is_reading_write = true; /* checking what value is reading */
+        // Then add it to Modules' storage
+        insert_item(ht, coord->name, coord);
 
-        for (i = 4; ans[i] != '\n'; i++) {
-            // If space is encountered then reading write-end value is done
-            // Start reading read-end (ahaha) value
-            if (ans[i] == ' ') {
-                is_reading_write = false;
-                j = 0;
-            }
-
-            // Copy write-end
-            if (is_reading_write) {
-                write_end[j++] = ans[i];                   
-            // Copy read-end
-            } else {
-                read_end[j++] = ans[i];
-            }
-        }
-        
-        // TODO: Update communication info in storage
         close(sfd);
-
         return 0;
     }
     // It means module with name already exist
     // Report about it and return error
-    else if (strncmp(ans, "IKNOW", 5) == 0) {
+    else if (strncmp(ans, ALREADYEXISTMSG, ALREADYEXISTMSGSIZE) == 0) {
         fprintf(stderr, "Module with that name already exist\n"); 
         close(sfd);
 
@@ -202,7 +297,6 @@ connect_to_coordinator(char *name, char *addr, char *port) {
 
         return -1;
     }
-
 }
 
 /*
@@ -210,5 +304,82 @@ connect_to_coordinator(char *name, char *addr, char *port) {
  */
 int
 init_modules_storage() {
+    // Initialize new Hash table with size defined by INITHASHTBLSIZE
+    ht = new_hash_table(INITHASHTBLSIZE); 
     return 0;
+}
+
+/*
+ * Get fds from answer
+ */
+void
+get_fds(char *msg, msg_len msg_term, char *write_end, char *read_end) {
+    msg_len i, j = 0;
+
+    // Checking what value is reading:
+    // true  - write_end
+    // false - read_end
+    bool is_reading_write = true;
+
+    for (i = msg_term; ans[i] != '\n'; i++) {
+        // If space is encountered then reading write-end value is done
+        // Start reading read-end (ahaha) value
+        if (ans[i] == ' ') {
+            is_reading_write = false;
+            j = 0;
+        }
+
+        // Read write-end
+        if (is_reading_write) {
+            write_end[j++] = ans[i];                   
+        } 
+        // Read read-end
+        else if (!is_reading_write) {
+            read_end[j++] = ans[i];
+        }
+    }
+}
+
+/*
+ * Set exit handler
+ */
+int
+set_exit_handler() {
+    // Here will be all needed info for sighandling
+    struct sigaction act; 
+
+    act.sa_handler = exit_handler; /* set handler */
+    sigemptyset(&act.sa_mask);     /* empty mask */
+    act.sa_flags = 0;              /* no flags are needed */
+
+    // Set actiong for SIGTERM
+    // If error, return -1
+    if (sigaction(SIGTERM, &act, NULL) == -1) {
+        perror("Error while setting actiong for SIGTERM");
+        return -1;
+    }
+
+    return 0;
+}
+
+/*
+ * Exit handler
+ */
+void
+exit_handler(int signum) {
+    // If we get SIGTERM, then shutdown module
+    if (signum == SIGTERM) {
+        shutdown_module();
+    }
+}
+
+/*
+ * Shutdown module
+ * Notify Coordinator about termination
+ * Close active connections
+ * Clear modules' data storage
+ * Clear event_base struct
+ */
+void
+shutdown_module() {
 }
--- src/libcoord.h
+++ src/libcoord.h
@@ -7,8 +7,28 @@
 #ifndef LIBCOORD_H
     #define LIBCOORD_H
         /* defines */
-        #define CONTHANDL 0 /* continue handling messages */
-        #define STOPHANDL 1 /* stop handling messages */ 
+        // Cooordinator's name
+        #define COORD     "coordinator" /* Coordinator's name */
+
+        // Handler's defines
+        #define CONTHANDL 0             /* continue handling messages */
+        #define STOPHANDL 1             /* stop handling messages */ 
+
+        // Messages for communicating with Coordinator
+        #define INITMSG         "IAM"   /* "Hello" msg */
+        #define ALREADYEXISTMSG "IKNOW" /* Decline new module */
+        #define ACEXISTMSG      "LOL"   /* Accept new module */
+        #define REQCONMSG       "KUKU"  /* Request connection */
+        #define ACCONMSG        "YOPTA" /* Accept connection's request */
+        #define DECCONMSG       "NOPE"  /* Decline connection's request */
+
+        // Messages' sizes
+        #define INITMSGSIZE         3
+        #define ALREADYEXISTMSGSIZE 5
+        #define ACEXISTMSGSIZE      3
+        #define REQCONMSGSIZE       4
+        #define ACCONMSGSIZE        5
+        #define DECCONMSGSIZE       4
 
         /* typedefs */
         typedef unsigned int mod_id;
@@ -23,16 +43,16 @@
         } Message;
         // Struct with module's info
         typedef struct Module {
-            mod_id id;         /* module's id */
-            char   *name;      /* module's name */
-            int    write_end;  /* module's write-end */
-            int    read_end;   /* module's read-end */
+            mod_id   id;         /* module's id */
+            char     *name;      /* module's name */
+            int      write_end;  /* module's write-end */
+            int      read_end;   /* module's read-end */
         } Module;
 
         /* functions */
         Module*  get_module_by_name(char *name);                   /* Get modules' information by name */
-        int      init_module(char *name, char *addr, char *port);                                    /* Initialize module */
-        int      connect_to_module(Module *mod);                   /* Connect to module */
+        int      init_module(char *name, char *addr, char *port);  /* Initialize module */
+        int      connect_to_module(char *mod);                     /* Connect to module */
         int      send_msg(Module *mod, char *buf, msg_len len);    /* Send message to module  */
         int      handle(int (*handler)(Message *));                /* Handle incoming messages */
         Message* get_msg();                                        /* Get any incoming message */
